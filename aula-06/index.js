c7b533358c61591bbe756d0c8939210b2f4f8951
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-02-18 10:50:15 -0300

    aula 6

--- aula-06/index.js
+++ aula-06/index.js
@@ -1,1 +1,26 @@
-// Aprendendo Vari
+// Aprendendo Variáveis
+
+/* 
+-> var
+-> let
+-> const
+*/
+
+var pote = 'bombom';
+alert(pote)
+
+//
+
+var a = 5;
+var b = 10;
+var c = a + b;
+alert(c);
+
+// OU por declaraç
+
+var a,b,c;
+
+a = 5;
+b = 10;
+c = a + b;
+alert(c);
