77651e1bd5cbd49d3835eca323fba40e59945d07
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-02-19 08:21:38 -0300

    class seven

---
+++ aula-07/index.js
@@ -0,0 +1,30 @@
+/* 
+Aprendeno Operadores
+
+1) Operadores Aritméticos (matemática)
+2) Operadores de Atribuição
+3) Operadores de Sequência
+4) Operadores de Comparação
+5) Operador Condicional (Ternário)
+6) Operadores Lógicos
+
+São Usados os Sinais: + - * / = == === ++ -- += -= && || ! ...
+*/
+
+var num1, num2, soma, sub, multi, divi;
+
+num1 = 10;
+num2 = 20;
+
+// Somente para numbers
+soma = num1 + num2;
+// alert(soma) / 30
+
+sub = num1 - num2;
+// alert(sub) / 10
+
+multi = num1 * num2;
+// alert(multi) / 200
+
+divi = num2 / num1;
+// alert(divi) / 2 
