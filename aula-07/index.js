c7858f50e053bf32aff1b0c95d0c912ebdea3a19
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-02-20 13:03:51 -0300

    class 7 finished

--- aula-07/index.js
+++ aula-07/index.js
@@ -39,7 +39,7 @@ divi = num1 / num2;
 // alert(num1) / 10
 
 incremento = ++num1; // incremento = --num1;
-alert('O incremento adiciona ao valor + ou - 1 e salva na base: ' + num1)
+// ('O incremento adiciona ao valor + ou - 1 e salva na base: ' + num1)
 
 // alert(num1) / Passa a ser  11
 
@@ -63,3 +63,27 @@ palavra2 = 'Quente';
 frase = palavra1 + palavra2
 // alert(frase) / Cachorro Quente
 
+// 4) Operadores de Comparação ( == === != !== > <) irá retorna true or false
+
+var numero1, numero2, comparar;
+
+numero1 = 10;
+numero2 = '10';
+
+comparar = (numero1 == numero2) // quer saber se os numeros são iguais idependente dos tipos já o ( === ) difere os tipos retornando false
+// alert(comparar) / true
+
+var numero3, numero4, comparar2;
+
+numero3 = 8;
+numero4 = '8';
+
+comparar2 = ( numero3 != numero4) // quer saber se os valores são diferentes idependente dos tipos já o ( !== ) difere os tipos retornando true
+// alert(comparar2) / true
+
+var test1, test2, total;
+
+test1 = 50;
+test2 = 20;
+total = (test1 > test2) // ( > ) Quer saber se o test1 é maior que o test2. ( < ) Quer saber se é menor. já o ( >= <= ) maior ou igual / menor ou igual 
+// alert(total) / true
