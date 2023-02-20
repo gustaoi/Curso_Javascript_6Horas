7eb122d547356e3fd3586f3ea7325c80b59a35a9
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-02-20 09:16:22 -0300

    class 07

--- aula-07/index.js
+++ aula-07/index.js
@@ -11,7 +11,7 @@ Aprendeno Operadores
 São Usados os Sinais: + - * / = == === ++ -- += -= && || ! ...
 */
 
-// 1) Operadores Aritméticos
+// 1) Operadores Aritméticos ( +  -  *  /  )
 
 var num1, num2, soma, sub, multi, incremento;
 
@@ -29,14 +29,37 @@ sub = num1 - num2;
 multi = num1 * num2;
 // alert(multi) / 200
 
-divi = num2 / num1;
-// alert(divi) / 2 
+divi = num1 / num2;
+// alert(divi) / 0,5 
+
+/* 2) Operadore de Atribuição ( ++ -- += -= *= /= ) */
 
 // Incremento
 
 // alert(num1) / 10
 
 incremento = ++num1; // incremento = --num1;
-alert('O incremento adiciona o valor + 1 e salva na base: ' + num1)
+alert('O incremento adiciona ao valor + ou - 1 e salva na base: ' + num1)
 
 // alert(num1) / Passa a ser  11
+
+// Incremneto de soma, sub, div, multi
+
+num1 += num2; // num1 -= num2 - 10
+// alert(num1) / 30
+
+num1 *= num2; // num1 /= num2 - 0,5
+// alert(num1) / 200
+
+/* 3) Operadores de Sequência ( + com strings ) */
+
+// Concatenação Junta duas variaveis'String'
+
+var palavra1, palavra2, frase;
+
+palavra1 = 'Cachorro ';
+palavra2 = 'Quente';
+
+frase = palavra1 + palavra2
+// alert(frase) / Cachorro Quente
+
