fe921a8b304726237dafde67aa70f67f47c927bc
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-02-22 19:49:42 -0300

    class 08

--- aula-08-function/index.js
+++ aula-08-function/index.js
@@ -1,5 +1,6 @@
 // Aprendendo Functions
 
+// FUNÇÃO DE SOMA
 function soma(valor1,valor2) {
     return valor1 + valor2
 }
@@ -8,6 +9,7 @@ var total = soma(20,10)
 
 document.getElementById('text').innerHTML = total
 
+// FUNÇÃO DE COTAÇÃO DO DOLAR
 function realParaDolar(real,cotacaoDoDolar) {
     return real * cotacaoDoDolar;
 }
@@ -18,3 +20,17 @@ var cotacao = 5.8;
 var realDolar = realParaDolar(valorReal,cotacao);
 
 document.getElementById('text').innerHTML = 'O valor em real é R$: ' + valorReal + 'O valor em dolar é U$: ' + realDolar;
+
+// FUNÇÃO POR ONCLICK
+function alertHello(params) {
+    alert('Olá Pessoal')
+}
+
+// FUNÇÃO POR VARIAVEIS
+function paraCelsius(valorFahrenheit) {
+    return (5/9) * (valorFahrenheit - 32);
+}
+
+var x = paraCelsius(77)
+
+alert(`A temperatura é de ${x} graus Celsius`)
