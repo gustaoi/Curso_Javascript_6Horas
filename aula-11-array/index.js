56ebc69c7ec5b9dfbdd09824029283c117a9f1ab
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-03-04 14:50:40 -0300

    class 11 finished

--- aula-11-array/index.js
+++ aula-11-array/index.js
@@ -1,1 +1,24 @@
 // APRENDENDO ARRAY
+
+/* 
+Array é uma maneira de armazenar varios valores em uma única variavel
+
+const lista = [arroz, feijão, macarrão, ovos]
+
+lista[0] = arroz
+lista[1] = feijão
+*/
+
+const lista = []; // ou const lista = new array(arroz, ...)
+
+lista[0] = 'arroz';
+
+//console.log(lista)
+
+lista.push = 'feijão'
+
+console.log(lista)
+
+const pessoa = ['joão', 'gustavo', 20] // array ou matriz
+
+const pessoa = {nome:'joão', sobrenome:'Gustavo', idade:20} // objetos
